471f9eb629c95abcd3666e0fe78f386bd9a2840a
Ubuntu <ubuntu@ip-172-31-79-40.ec2.internal>
2021-01-11 05:51:09 +0000

    Initial Commit

---
+++ CarMain.js
@@ -0,0 +1,102 @@
+/**
+ * Copyright (c) 2020
+ *
+ * The TryCatchPop file adds integer added by the 
+ * user to the stack array and it pops it off the stack. It also checks
+ * whether the stack is empty or not and doesn't crash.
+ * 
+ * @author Wajd Mariam <wajd.mariam@mths.ca>
+ * Version 1.0
+ * Created on : 2020/12/13
+ */
+ 
+// Importing the class WajdStack from another file
+const Vehicles = require("./Vehicles");
+
+// Imports prompt
+const prompt = require('prompt-sync')({sigint: true});
+
+
+// Declaring variables used:
+let licensePlateCar = "";
+let carColorUser = "";
+let licensePlateCar1Change = "";
+let colorCar1Change = "";
+let speedChange = 0;
+
+// Try Catch Statement:
+try {
+  
+  // Input / Process / Output
+  
+  // Car #1
+  
+  // Getting license plate number for car#1:
+  licensePlateCar = prompt(`Enter your license plate number of your first car: `);
+  
+  // Getting car color of the first car:
+  carColorUser = prompt(`Enter the color of your first car: `);
+  
+  // // Printing out the first car's information:
+  let firstCar = new Vehicles(licensePlateCar, carColorUser); 
+  console.log(``);
+  console.log(`Here are your first car's information: `);
+  console.log(`License plate #: ${firstCar.licensePlateGetter()}.`);
+  console.log(`Car color: ${firstCar.colorGetter()}`);
+  console.log(``);
+  
+  // Car #2
+  
+  // Getting license plate number for car#2:
+  licensePlateCar = prompt(`Enter your license plate number of your second car: `);
+  
+  // Getting car color of the first car:
+  carColorUser = prompt(`Enter the color of your second car: `);
+  
+  // // Printing out the first car's information:
+  let secondCar = new Vehicles(licensePlateCar, carColorUser); 
+  console.log(``);
+  console.log(`Here are your second car's information: `);
+  console.log(`License plate #: ${secondCar.licensePlateGetter()}.`);
+  console.log(`Car color: ${secondCar.colorGetter()}`);
+  
+  // Chaning cars information:
+  // Changing car#1 licence plate number:
+  console.log(``);
+  console.log(`Your license plate on your first car is outdated. You need a new plate!`);
+  console.log(`Please enter a valid license plate number: `);
+  licensePlateCar1Change = prompt(``);
+  firstCar.licensePlateSetter(licensePlateCar1Change);
+  console.log(`Your new license plate number: ${firstCar.licensePlateGetter()}`);
+  
+  // Changing car#1 color:
+  console.log(``);
+  console.log(`Your first car's body is rusty. You need a new paint job!`);
+  console.log(`Please enter a color to paint your car:`);
+  colorCar1Change = prompt(``);
+  firstCar.colorSetter(colorCar1Change);
+  console.log(`Your new car color: ${firstCar.colorGetter()}`);
+  
+  // Chaning cars speeds:
+  // Acceleration:
+  console.log(``);
+  console.log(`Go take your second car for a test ride`);
+  console.log(`How fast do you want to go? in KM/H`);
+  speedChange = prompt(``);
+  // Returning speed:
+  console.log(`Your car is travelling at ${secondCar.accelerationCalculation(speedChange)} KM/H`);  
+  
+  // Deceleration:
+  console.log(``);
+  console.log(`There are traffic up head!`);
+  console.log(`SLOW DOWN!`);
+  console.log(`How much do you want to decrease your speed? in KM/H`);
+  speedChange = prompt(``);
+  // Returning speed:
+  console.log(`Your car is travelling at ${secondCar.decreaseCalculation(speedChange)} KM/H`); 
+  
+  
+// Error Catch Statement:
+} catch(err) {
+    console.log (`ERROR. Your car isn't able to do such thing!`);
+}
---
+++ Vehicles.js
@@ -0,0 +1,73 @@
+/**
+ * Copyright (c) 2020
+ *
+ *  The WajdStack3 Class creates a stack, and methods to use 
+ * by the PeekClear.js program.
+ * 
+ * @author Wajd Mariam <wajd.mariam@mths.ca>
+ * Version 1.0
+ * Created on : 2020/12/13
+ */
+ 
+class Vehicles {
+    
+  // Constructors that defines stack array
+  constructor(licensePlateInput, colorInput) {
+    this.licensePlate = licensePlateInput;
+    this.color = colorInput;
+    this.speed = 0;
+    this.maxSpeed = 240;
+  }
+  
+  // Setter for license plate combination.
+  licensePlateSetter(inputLicensePlate) {
+    this.licensePlate = inputLicensePlate;
+  }
+  
+  // Getters for license plate combination.
+  licensePlateGetter() {
+    return this.licensePlate;
+  }
+  
+  // Setter for color.
+  colorSetter(carColorUser) {
+    this.color = carColorUser;
+  }
+  
+  // Getter for color.
+  colorGetter() {
+    return this.color;
+  }
+  
+  // Method that calculates the car speed while accelerating.
+  accelerationCalculation(speedIncrease) {
+    // converting all variables from strings to integers.
+    this.maxSpeed = parseInt(this.maxSpeed, 10);
+    this.speed = parseInt(this.speed, 10);
+    speedIncrease = parseInt(speedIncrease, 10);
+    // Proceeding
+    if ((this.speed + speedIncrease) > this.maxSpeed) {
+      throw null;
+    } else {
+      this.speed = this.speed + speedIncrease;
+    }
+    return this.speed;
+  }
+  
+  // Method that calculates the car speed while deceleration.
+  decreaseCalculation(speedDecrease) {
+    // converting all variables from strings to integers.
+    this.speed = parseInt(this.speed, 10);
+    speedDecrease = parseInt(speedDecrease, 10);
+    // Proceeding
+    if ((this.speed - speedDecrease) < 0) {
+      throw null;
+    } else {
+      this.speed = this.speed - speedDecrease;
+    }
+    return this.speed;
+  }
+}
+
+// Exporting the class
+module.exports = Vehicles;
